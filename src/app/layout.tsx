752543645fa7db4c0cda923cdb2d3c2e542e94c1
Nitin Kaushik <nitinkaushikdesigns@gmail.com>
2025-12-08 22:02:39 +0530

    More css

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,12 @@
 import './globals.css';
 import Header from '@/components/Header';
+import { Rubik } from 'next/font/google';
+
+const rubik = Rubik({
+  subsets: ['latin'],
+  weight: ['300', '400', '500', '600', '700', '800', '900'],
+  display: 'swap',
+});
 
 export default function RootLayout({
   children,
@@ -7,7 +14,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
+    <html lang="en" className={rubik.className}>
       <body>
         {/* ✅ GLOBAL HEADER */}
         <Header />
