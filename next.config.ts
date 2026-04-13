d6797b4e08224b8f4f8d16b1effbf05b0317a8fe
Nitin <nitinkaushikdesigns@gmail.com>
2026-04-13 21:52:29 +0530

    212. v2 fix nextconfig

--- next.config.ts
+++ next.config.ts
@@ -1,39 +1,12 @@
-const nextConfig = {
+import type { NextConfig } from 'next';
+
+const nextConfig: NextConfig = {
   output: 'export',
-  trailingSlash: true, // ✅ THIS FIXES THE 403 ERROR
+  trailingSlash: true, // Fixes the 403 ERROR
   images: {
     unoptimized: true,
   },
   reactStrictMode: true,
-
-  async redirects() {
-    return [
-      {
-        source: '/:path*',
-        has: [
-          {
-            type: 'host',
-            value: 'www.fincado.com',
-          },
-        ],
-        destination: 'https://fincado.com/:path*',
-        permanent: true,
-      },
-    ];
-  },
-  async headers() {
-    return [
-      {
-        source: '/sitemap.xml',
-        headers: [
-          {
-            key: 'Content-Type',
-            value: 'application/xml; charset=utf-8',
-          },
-        ],
-      },
-    ];
-  },
 };
 
-module.exports = nextConfig;
+export default nextConfig;
